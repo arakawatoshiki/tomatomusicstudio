b9a61b47599e2cdd74c227e9d587e5dde9c7719c
arakawatoshiki <73926988+arakawatoshiki@users.noreply.github.com>
2020-11-26 18:35:18 +0900

    EventListener changed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// ハンバーガーアイコン
 {
   const open = document.getElementById('open');
   const overlay = document.querySelector('.overlay');
@@ -14,5 +15,25 @@
     overlay.classList.remove('show');
     open.classList.remove('hide');
   });
+  
+  $('#page a[href]').on('click', function(event) {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
 }
+
+
+// ページスクロール
+$(function(){
+  $(window).scroll(function (){
+      $('.effect-fade').each(function(){
+          var elemPos = $(this).offset().top;
+          var scroll = $(window).scrollTop();
+          var windowHeight = $(window).height();
+          if (scroll > elemPos - windowHeight){
+              $(this).addClass('effect-scroll');
+      }
+    });
+  });
+});
 
